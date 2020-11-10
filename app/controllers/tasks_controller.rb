6f495ae99dad317617b0834f117443a7cc6537d6
clara wolff <cwo@systemli.org>
2020-11-10 15:07:37 +0100

    completed backend

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,9 +18,28 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    tasks = Task.all
+    @task = tasks.find(params[:id])
+  end
+
+  def update
+    tasks = Task.all
+    @task = tasks.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    tasks = Task.all
+    @task = tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
